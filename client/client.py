64bfe520c3895dbddeefc86fcec0cfd51983d586
Deltavion <89981881+Deltavion@users.noreply.github.com>
2022-06-11 22:43:58 +0200

    Frontend BETA
    NOT STABLE VERSION

--- client/client.py
+++ client/client.py
@@ -2,22 +2,9 @@ import socket
 import threading
 import os
 import ast
+import gui
+import time
 
-#=====================================================================functions
-def send(msg, socket):
-    # send the lenght of the msg, on 255 bytes
-    socket.send(str(len(msg)).encode())
-    socket.send(msg.encode())  # send the msg
-
-
-def handle_input():
-    global running
-    while running:
-        msg = input()
-        if msg:
-            send(msg, CLIENT_SOCKET)
-        if msg == "!quit":
-            running = False
 #=====================================================================constants
 PORT = 56789
 SERVER_IP = "192.168.1.84"
@@ -26,33 +13,61 @@ NAME = os.getenv("USERNAME")
 # create a socket, socket type and mode
 CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#=====================================================================variables
-running = True
+#=====================================================================functions
+def backend():
+    global app
+    print("backend")
+    time.sleep(3)
+    app.refresh_client()
+    print("back")
 
-#==========================================================================main
-# connection
-CLIENT_SOCKET.connect(SERVER_ADDR)
-# sending name
-CLIENT_SOCKET.send(NAME.encode())
-# receiving dns
-dns_str_lenght = CLIENT_SOCKET.recv(255).decode()
-if dns_str_lenght:
-    dns_str = CLIENT_SOCKET.recv(int(dns_str_lenght)).decode()
-    dns = ast.literal_eval(dns_str)
+    # # connection
+    # CLIENT_SOCKET.connect(SERVER_ADDR)
+    # # sending name
+    # CLIENT_SOCKET.send(NAME.encode())
+    # # receiving dns
+    # dns_str_lenght = CLIENT_SOCKET.recv(255).decode()
+    # if dns_str_lenght:
+    #     dns_str = CLIENT_SOCKET.recv(int(dns_str_lenght)).decode()
+    #     dns = ast.literal_eval(dns_str)
 
 
-print(f"Successfully connected to {SERVER_IP}")
-print(f"Client IP : {socket.gethostbyname(socket.gethostname())}")
-print(f"Client name : {NAME}")
-print("=============================================================")
-#CLIENT_SOCKET.recv(255)
+    # print(f"Successfully connected to {SERVER_IP}")
+    # print(f"Client IP : {socket.gethostbyname(socket.gethostname())}")
+    # print(f"Client name : {NAME}")
+    # print("=============================================================")
 
-thread = threading.Thread(target=handle_input)
-thread.start()
-print(f"LOG {dns}")
-while running:
+    # print(f"LOG {dns}") #test
+
+    # running = True
+    # while running:
+    #     handle_message()
+
+
+
+def send(msg, socket):
+    # send the lenght of the msg, on 255 bytes
+    socket.send(str(len(msg)).encode())
+    socket.send(msg.encode())  # send the msg
+
+
+def handle_input():
+    msg = input()
+    if msg:
+        send(msg, CLIENT_SOCKET)
+    if msg == "!quit":
+        pass
+
+def handle_message():
     msg_lenght = CLIENT_SOCKET.recv(255).decode()
     if msg_lenght:
         msg = CLIENT_SOCKET.recv(int(msg_lenght)).decode()
         print(msg)
 
+#=====================================================================variables
+
+
+#==========================================================================main
+app = gui.SocketApp()
+threading.Thread(target=backend).start()
+app.run()
---
+++ client/gui.py
@@ -0,0 +1,44 @@
+import kivy
+from kivy.app import App
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.recycleview import RecycleView
+
+kivy.require("2.0.0")
+
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+
+
+class View(GridLayout):
+    def __init__(self, **kwargs):
+        super(View, self).__init__(**kwargs)
+        self.cols = 2
+
+        msgs = RV()
+        msgs.data = [{"text": "Bienvenue sur le terminal SocketAPP"}]
+        self.add_widget(msgs)
+
+        self.clients = RV()
+        self.clients.data = [{"text": "void"} for i in range(20)]
+        self.add_widget(self.clients)
+
+    def refresh_client(self):
+        self.clients.refresh_from_data(data=[{"text": "coucou"}])
+
+
+class SocketApp(App):
+    def build(self):
+        self.mainView = View()
+        return self.mainView
+
+    def refresh_client(self):
+        self.mainView.refresh_client()
+
+
+
+if __name__ == '__main__':
+    SocketApp().run()
